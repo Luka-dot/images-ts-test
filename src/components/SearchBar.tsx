5411a076b7829d9938da548ed72f271ffa3d246d
Luka-dot <79lukask@gmail.com>
2021-02-16 23:29:48 -0500

    search bar component started

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Search } from 'semantic-ui-react'
+import { Form, Container, Label } from 'semantic-ui-react'
+import './SearchBar-styles.css';
 
 class SearchBar extends React.Component {
+    state = { searchText: String };
+
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => void (
+        console.log(e.target.value),
+        this.setState({ searchText: e.target.value})
+    );
 
     render() {
         return (
-            <div>
-                <Search 
+            <div className="search-container">
+                <Label 
+                    pointing='below' 
+                    color='teal' 
                     size="large"
-                    placeholder="enter your search"
-                />
+                >
+                Image Search
+                </Label>
+                <Form 
+                    size="large"
+                    placeholder="enter your search"    
+                >
+                    <input 
+                        placeholder="enter your search" 
+                        onChange={this.handleChange} 
+                        type="text"
+                    />
+                </Form>
             </div>
         )
     }
